f5eb2e96c856eab792584b99e0c788d82156d8fc
Johnny Bouder <jbouder@metrostar.com>
2024-12-30 14:34:18 -0500

    Update to use action instead of react-query for making async call.

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,8 +2,8 @@ import { Spinner } from '@metrostar/comet-extras';
 import { Card, CardBody } from '@metrostar/comet-uswds';
 import { Spacecraft } from '@src/types/spacecraft';
 import axios from '@src/utils/axios';
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import { AxiosError } from 'axios';
+import React, { useEffect, useState, useTransition } from 'react';
 import ErrorNotification from '../../components/error-notification/error-notification';
 import useAuth from '../../hooks/use-auth';
 import { DashboardBarChart } from './dashboard-bar-chart/dashboard-bar-chart';
@@ -12,23 +12,27 @@ import { DashboardTable } from './dashboard-table/dashboard-table';
 
 export const Dashboard = (): React.ReactElement => {
   const { isSignedIn } = useAuth();
-  const {
-    isLoading,
-    error,
-    data: items,
-  } = useQuery<Spacecraft[], { message: string }>({
-    queryKey: ['dashboard'],
-    queryFn: () =>
-      axios
-        .get('/spacecraft')
-        .then((response) => {
-          return response.data;
-        })
-        .then((data) => {
-          return data.items;
-        }),
-    enabled: isSignedIn,
-  });
+  const [items, setItems] = useState<Spacecraft[] | undefined>(undefined);
+  const [error, setError] = useState<AxiosError | undefined>(undefined);
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      startTransition(() => {
+        axios
+          .get('/spacecraft')
+          .then((response) => {
+            return response.data;
+          })
+          .then((data) => {
+            setItems(data.items);
+          })
+          .catch((error: AxiosError) => {
+            setError(error);
+          });
+      });
+    }
+  }, [isSignedIn]);
 
   return (
     <div className="grid-container">
@@ -64,7 +68,7 @@ export const Dashboard = (): React.ReactElement => {
       </div>
       <div className="grid-row">
         <div className="grid-col">
-          {isLoading ? (
+          {isPending ? (
             <Spinner id="spinner" type="small" loadingText="Loading..." />
           ) : (
             <DashboardTable items={items} />
